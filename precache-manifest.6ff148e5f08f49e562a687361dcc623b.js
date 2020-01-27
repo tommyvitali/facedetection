e1164bd6729540628d1b5e61b24d0d4d11eee348
tommyvitali <56254943+tommyvitali@users.noreply.github.com>
2020-01-27 13:07:23 +0100

    Updates

---
+++ precache-manifest.6ff148e5f08f49e562a687361dcc623b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eef44642a67f275c8040b4acfc71079",
+    "url": "/facedetection/index.html"
+  },
+  {
+    "revision": "5015ac0071ac65430878",
+    "url": "/facedetection/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "42cc0119b7aac209d157",
+    "url": "/facedetection/static/css/main.85c896de.chunk.css"
+  },
+  {
+    "revision": "5015ac0071ac65430878",
+    "url": "/facedetection/static/js/2.7817c2c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/facedetection/static/js/2.7817c2c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42cc0119b7aac209d157",
+    "url": "/facedetection/static/js/main.ccbb4d42.chunk.js"
+  },
+  {
+    "revision": "318a39ceae7b5ef2ef86",
+    "url": "/facedetection/static/js/runtime-main.00d1d2ef.js"
+  }
+]);
